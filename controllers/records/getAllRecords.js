fbc81a98a7ce882b16e3782578ac71e1ab39901e
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-01-26 16:52:47 +0200

    fix bug with total duration

--- controllers/records/getAllRecords.js
+++ controllers/records/getAllRecords.js
@@ -4,11 +4,17 @@ const getAllRecords = async (req, res, next) => {
   try {
     const { limit = 2, page = 1} = req.query
     const { docs: records, ...rest } = await Record.paginate({},{limit,page})
+    const totalDuration = await Record.aggregate([
+      {
+        $group: { _id: " ", total: { $sum: "$duration" } },
+      },
+    ])
 
     res.json({
       status: '✔️ Success',
       code: 200,
-      data: { records, ...rest },
+
+      data: { totalDuration:totalDuration[0],records, ...rest },
     })
   } catch (error) {
     next(error)
